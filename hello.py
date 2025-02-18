bfcf7a2b2594816b9b0100c07e8a3ebe66dfdfb5
aqkhan222 <Abdul Qadeer Khan@elitebook840.namal.edu.pk>
2025-02-18 15:39:52 +0500

    hiii

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Define a list of numbers  
+numbers = [1, 2, 3, 4, 5]  
+
+# Calculate the sum of the numbers  
+total_sum = sum(numbers)  
+
+# Print the result  
+print("The sum of the numbers is:", total_sum)
